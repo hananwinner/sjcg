afd3e545fbc35ef79e4583a2bb812955905ac54e
Hanan Wiener <Hanan Wiener>
2016-05-08 11:32:57 +0300

    master: fixing help message.

--- syncano_java_class_generator.py
+++ syncano_java_class_generator.py
@@ -215,7 +215,7 @@ class SyncanoJavaClassGenerator:
         return schema
 if __name__ == '__main__':
 
-    parser = argparse.ArgumentParser(prog="sjcg",description="Generate Java class for Syncano class")
+    parser = argparse.ArgumentParser(prog="syncano_java_class_generator.py",description="Copyright 2016 Hanan Wiener.\nGenerate Java class for Syncano class.", )
     parser.add_argument("api_key",metavar="API-KEY", type=str, help="The API-KEY used to retrieve class metadata." )
     parser.add_argument("class_url",metavar="CLASSES-URL", type=str, help="Full URL to the Syncano classes endpoint.")
     parser.add_argument("dest_folder", metavar="DESTINATION-FOLDER",type=str,help="Folder path to create the Java classes at.")
